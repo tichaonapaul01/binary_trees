1100519522779660d8cbd5a3d1855ba8287579b5
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-08-04 22:14:34 -0700

    initial commit

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
